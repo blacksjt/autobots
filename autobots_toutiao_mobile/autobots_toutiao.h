758601eab5eeadb18f3b635dc3336a538ef225ed
Administrator <Administrator@USER-20170110ZO>
2017-02-10 17:47:11 +0800

    2017.2.10

--- autobots_toutiao_mobile/autobots_toutiao.h
+++ autobots_toutiao_mobile/autobots_toutiao.h
@@ -9,89 +9,117 @@
 struct AccountParam
 {
 public:
-  AccountParam(QString name, QString password)
-  {
-    _id = name;
-    _password = password;
-  }
+	AccountParam(QString name, QString password)
+	{
+		_id = name;
+		_password = password;
+	}
 
 public:
-  QString _id;
-  QString _password;
+	QString _id;
+	QString _password;
 };
 
 typedef QList<AccountParam> AccountList;
 
+struct DeviceParam
+{
+public:
+	DeviceParam(QString uuid, QString did, QString agent, QString device_type)
+	{
+		_uuid = uuid;
+		_did = did;
+		_useragent = agent;
+		_device_type = device_type;
+	}
+
+	DeviceParam()
+	{
+
+	}
+public:
+	QString _uuid;
+	QString _did;
+	QString _useragent;
+	QString _device_type;
+};
+
+typedef QList<DeviceParam> DeviceParamList;
+
+
 class autobots_toutiao : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    autobots_toutiao(QWidget *parent = 0);
-    ~autobots_toutiao();
+	autobots_toutiao(QWidget *parent = 0);
+	~autobots_toutiao();
 
 private:
 
-private slots:
-  void onStart();
-  void onPause();
-  //void onPostIdReday(QString);
-  void onAddCommentID();
-  //void onCsrfTokenReday(QString str);
-  //void onLoginsucceed(QString id);
-  void onActFromTxt();
-  void onActImportComment();
-  void onActClearComments();
-  void onActClearAccounts();
-  //void onClientIdReday(QString _id);
-  //void onFatieSucceed(QString comment, double id);
+	private slots :
+		void onStart();
+	void onPause();
+	//void onPostIdReday(QString);
+	void onAddCommentID();
+	//void onCsrfTokenReday(QString str);
+	//void onLoginsucceed(QString id);
+	void onActFromTxt();
+	void onActImportComment();
+	void onActClearComments();
+	void onActClearAccounts();
+	//void onClientIdReday(QString _id);
+	//void onFatieSucceed(QString comment, double id);
 private:
-  int LogInbySina();
-  //QNetworkReply* LogInbyRenren(const QString& name, const QString& password);
-  bool DoPostFatie(const QString& content);
-  void GetUerInfo();
-  bool GetContent();
-  void GetConnection();
-  void LoginRequest();
-  bool RequestForRenren();
-  void CodeCheckForRenren();
-  bool AuthorByRenren(const QString& name, const QString& password);
-  void UpdateData();
-  void initialize();
-  void Logout();
-  bool CheckInput();
-  bool GetCsrfToken(const QByteArray& arr);
-  bool ProcessRedirectSSL(const QString& str);
-  bool ProcessRedirectGet(const QString& str);
-  bool GetPostId(const QByteArray& arr);
-  int ProcessRedirectLoginGet(const QString& str);
-  bool ProcessRedirectLoginGet2(const QString& str);
-  int ProcessRedirectLoginGetTemp(const QString& str);
-  int ProcessRedirectLoginGetTemp2(const QString& str);
-  bool GetFatieStatus(const QByteArray& byte_arr);
-  void FatieSucceed(const QString& comment,const QString& id);
-  bool NeedValidateCode(const QString& name, QString& vcode, QString& code_sign);
+	int LogInbySina();
+	//QNetworkReply* LogInbyRenren(const QString& name, const QString& password);
+	bool DoPostFatie(const QString& content);
+	void GetUerInfo();
+	bool GetContent();
+	void GetConnection();
+	void LoginRequest();
+	bool RequestForRenren();
+	void CodeCheckForRenren();
+	bool AuthorByRenren(const QString& name, const QString& password);
+	void UpdateData();
+	void initialize();
+	void initialDevices();
+	void Logout();
+	bool CheckInput();
+	bool GetCsrfToken(const QByteArray& arr);
+	bool ProcessRedirectSSL(const QString& str);
+	bool ProcessRedirectGet(const QString& str);
+	bool GetPostId(const QByteArray& arr);
+	int ProcessRedirectLoginGet(const QString& str);
+	bool ProcessRedirectLoginGet2(const QString& str);
+	int ProcessRedirectLoginGetTemp(const QString& str);
+	int ProcessRedirectLoginGetTemp2(const QString& str);
+	bool GetFatieStatus(const QByteArray& byte_arr);
+	void FatieSucceed(const QString& comment, const QString& id);
+	bool NeedValidateCode(const QString& name, QString& vcode, QString& code_sign);
 
-  void WaitforSeconds(int nseconds);
+	void WaitforSeconds(int nseconds);
 private:
-    Ui::autobots_toutiaoClass ui;
-    toutiao_network* network;
-    bool control_status ;
-    QString m_client_id;
-    QString m_post_id;
-    QString m_csrf_token;
-    QString m_state_id;
-    bool m_code_online;
+	Ui::autobots_toutiaoClass ui;
+	toutiao_network network;
+	bool control_status;
+	QString m_client_id;
+	QString m_post_id;
+	QString m_csrf_token;
+	QString m_state_id;
+	bool m_code_online;
 private:
-    QString m_url;
-    QString m_news_id;
-    QString m_group_id;
-    int m_interval;
-    QStringList m_comment_list;
-    QList<QListWidgetItem*> m_comment_item_list;
-    AccountList m_account_list;
-    QList<int> m_account_row_list;
-    int m_account_order;
+	QString m_url;
+	QString m_news_id;
+	QString m_group_id;
+	int m_interval;
+	QStringList m_comment_list;
+	QList<QListWidgetItem*> m_comment_item_list;
+	AccountList m_account_list;
+	QList<int> m_account_row_list;
+	int m_account_order;
+
+	DeviceParamList m_devices_list;
 };
 
 #endif // AUTOBOTS_TOUTIAO_H
