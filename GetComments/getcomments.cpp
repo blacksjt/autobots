4924cef4ba176654d4654d9ff5e5bea7dc4090b7
shaojt <blacksjt@163.com>
2017-03-12 11:59:10 +0800

    20170312

--- GetComments/getcomments.cpp
+++ GetComments/getcomments.cpp
@@ -9,6 +9,14 @@
 #include "QJsonDocument"
 #include "QJsonObject"
 #include "QJsonArray"
+
+QString GetTimeStr()
+{
+	qint64 n = QDateTime::currentMSecsSinceEpoch();
+
+	return QString::number(n);
+}
+
 GetComments::GetComments(QWidget *parent)
     : QMainWindow(parent)
 {
@@ -67,7 +75,7 @@ void GetComments::startWork()
 	while (ncount < m_count && has_more)
 	{
 		has_more = GetSubComments(ncount, comments);
-		ncount += 20;
+		ncount += 19;
 	}
 
 	if (comments.isEmpty())
@@ -90,19 +98,19 @@ void GetComments::startWork()
 
 bool GetComments::GetSubComments(int& current_count, QStringList& cmts)
 {
-	QString url = QString("http://www.toutiao.com/api/comment/list/?group_id=%1&item_id=%2&offset=%3&count=20")
+	QString url = QString("http://is.snssdk.com/article/v1/tab_comments/?version_code=5.9.8&app_name=news_article&vid=674FB6B1-60E9-4315-88FC-AAC84BEFAB46&device_id=3135986566&channel=App%20Store&resolution=750*1334&aid=13&ab_version=111305,109656,101786,111547,101533,109464,110341,109893,109777,106784,97143,111342,101558,94042,105610,111814,105822,111317,111613,110304,108978,111799,108489,110718,110795,111417,98042,111606,105475&ab_feature=z2&ab_group=z2&openudid=0d919477efbefb99dfe7a02a2df34d9127ecc947&live_sdk_version=1.6.5&idfv=674FB6B1-60E9-4315-88FC-AAC84BEFAB46&ac=WIFI&os_version=9.3.5&ssmix=a&device_platform=iphone&iid=7730017535&ab_client=a1,f2,f7,e1&device_type=iPhone%206&idfa=86E011D2-C2DA-40CB-AB9D-DB1E1F9D668A&aggr_type=1&count=20&group_id=%1&item_id=%2&offset=%3&tab_index=0")
 		.arg(m_group_id, m_item_id, QString::number(current_count));
 
 	QNetworkAccessManager manager;
 	QNetworkRequest req;
 	req.setUrl(url);
-	req.setRawHeader("Cache-Control", "no-cache");
-	req.setRawHeader("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
+	req.setRawHeader("tt-request-time", GetTimeStr().toUtf8().data());
+	req.setRawHeader("Accept", "*/*");
 	req.setRawHeader("Connection", "Keep-Alive");
-	req.setRawHeader("X-Requested-With", "XMLHttpRequest");
-	req.setRawHeader("Accept-Language", "zh-CN");
-	req.setRawHeader("Host", "www.toutiao.com");
-	req.setRawHeader("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)");
+	//req.setRawHeader("X-Requested-With", "XMLHttpRequest");
+	req.setRawHeader("Accept-Language", "zh-Hans-CN;q=1");
+	req.setRawHeader("Host", "is.snssdk.com");
+	req.setRawHeader("User-Agent", "News/5.9.8 (iPhone; iOS 9.3.5; Scale/2.00)");
 
 	QNetworkReply* reply = manager.get(req);
 
@@ -149,23 +157,31 @@ bool GetComments::ExactComments(const QByteArray& rp_data, QStringList& comments
             // 查找data节点
             if (obj.contains("data"))
             {
-                QJsonObject data_obj = obj.take("data").toObject();
+				QJsonArray data_obj_arr = obj.take("data").toArray();
 
-                if (data_obj.contains("comments"))
-                {
-                    QJsonArray comments_arry = data_obj.take("comments").toArray();
-                    for (int i = 1; i < comments_arry.size(); ++i)
-                    {
-                        QJsonObject comment_obj = comments_arry.at(i).toObject();
-                        if (comment_obj.contains("text"))
-                        {
-                            QString c = comment_obj.take("text").toString();
-                            comments.push_back(c);
-                        }
-                    }
-
-                }
+				for (int i = 1; i < data_obj_arr.size(); ++i)
+				{
+					QJsonObject comment_obj = data_obj_arr.at(i).toObject();
+					if (comment_obj.contains("comment"))
+					{
+						QJsonObject comment_ = comment_obj.take("comment").toObject();
+						if (comment_.contains("text"))
+						{
+							QString c = comment_.take("text").toString();
+							if (c.contains("//@"))
+							{
+								c = c.left(c.indexOf("//@"));
+							}
+							comments.push_back(c);
+						}
+					}
+				}
             }
+
+			if (obj.contains("has_more"))
+			{
+				res = obj.take("has_more").toBool();
+			}
         }
     }
 
@@ -175,7 +191,7 @@ bool GetComments::ExactComments(const QByteArray& rp_data, QStringList& comments
     }
 	else
 	{
-		return true;
+		return res;
 	}
 
     // 写文件
